bbcb4d5e3c223b711418c162f6fa8cf2df80f129
tkckdnjs1 <testtkcld@gmail.com>
2025-03-13 17:16:37 +0900

    save 2025 03 13

---
+++ docs/js/main.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var anchors = document.querySelectorAll(".anchors ul li a");
+  var pageable = new Pageable("main", {
+      animation: 400,
+      onInit: updateAnchorColor,
+      onStart: updateAnchorColor,  // 스크롤 시작할 때 색상 변경
+      onFinish: updateAnchorColor  // 스크롤 종료 후에도 색상 유지
+  });
+
+  function updateAnchorColor(data) {
+      var index = data.index + 1;
+      var isEven = index % 2 === 0;
+      anchors.forEach(anchor => {
+          anchor.style.transition = "color 0.2s ease"; // 부드러운 전환
+          anchor.style.color = isEven ? "#000" : "#fff";
+      });
+  }
+});
